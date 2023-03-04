2c483ee5b1abb6b93dc196ab980abd8286291fd1
The Hieu <thehieu236@gmail.com>
2023-03-04 21:18:17 +0700

    update late

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,19 +1,61 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Loader, Card, FormField } from "../components";
 
 const Home = () => {
   const [loading, setLoading] = useState(false);
-  const [allPost, setAllPost] = useState(null);
+  const [allPosts, setAllPosts] = useState(null);
   const [searchText, setSearchText] = useState("");
+  const [searchedResults, setSearchedResults] = useState(null);
+  const [searchTimeout, setSearchTimeout] = useState(null);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+
+      try {
+        const response = await fetch("http://localhost:8080/api/v1/post", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+
+        if (response.ok) {
+          const result = await response.json();
+
+          setAllPosts(result.data.reverse());
+        }
+      } catch (error) {
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchPosts();
+  }, []);
 
   const RenderCards = ({ data, title }) => {
     if (data?.length > 0) {
-      return data.map((post) => {
-        <Card key={post._id} {...post}></Card>;
-      });
+      return data.map((post) => <Card key={post._id} {...post} />);
     }
 
     return <h2 className="mt-5 font-bold text-[#6449ff] text-xl">{title}</h2>;
+  };
+
+  const handleSearchChange = (e) => {
+    clearTimeout(searchTimeout);
+    setSearchText(e.target.value);
+
+    setSearchTimeout(
+      setTimeout(() => {
+        const searchResult = allPosts.filter(
+          (item) =>
+            item.name.toLowerCase().includes(searchText.toLowerCase()) ||
+            item.prompt.toLowerCase().includes(searchText.toLowerCase())
+        );
+        setSearchedResults(searchResult);
+      }, 500)
+    );
   };
 
   return (
@@ -28,7 +70,14 @@ const Home = () => {
         </p>
       </div>
       <div className="mt-16">
-        <FormField />
+        <FormField
+          labelName={"Search posts"}
+          type="text"
+          name={"text"}
+          placeholder="Search posts"
+          value={searchText}
+          handleChange={handleSearchChange}
+        />
       </div>
       <div className="mt-10">
         {loading ? (
@@ -40,7 +89,7 @@ const Home = () => {
             {searchText && (
               <h2>
                 Showing results for
-                <span className="text-[#222328]">{searchText}</span>
+                <span className="text-[#222328]">{` ${searchText}`}</span>
               </h2>
             )}
           </>
@@ -49,9 +98,9 @@ const Home = () => {
 
       <div className="grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3">
         {searchText ? (
-          <RenderCards data={["allPosts"]} title="No search results found" />
+          <RenderCards data={searchedResults} title="No search results found" />
         ) : (
-          <RenderCards data={["allPosts"]} title="no post found" />
+          <RenderCards data={allPosts} title="no post found" />
         )}
       </div>
     </section>
